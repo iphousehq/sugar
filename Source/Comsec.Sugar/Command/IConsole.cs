99f90ade69209ed7434ab0c7039f040a0f62e5d7
Chris Wood <chris@flipbit.co.uk>
2011-07-12 18:03:17 +0100

    Added Command Line Interface classes

---
+++ Source/Comsec.Sugar/Command/IConsole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Comsec.Lib.Console
+{
+    /// <summary>
+    /// Console class interface
+    /// </summary>
+    public interface IConsole
+    {
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="arg">The args.</param>
+        void WriteLine(string value, params object[] arg);
+
+        /// <summary>
+        /// Writes the line.
+        /// </summary>
+        /// <param name="eventName">Name of the event.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="arg">The arg.</param>
+        void WriteLine(string eventName, ConsoleColor color, string value, params object[] arg);
+
+        /// <summary>
+        /// Moves the cursor to line begining.
+        /// </summary>
+        void MoveCursorToLineBegining();
+
+        void ClearLine();
+
+        void WriteStatus(double percent, ConsoleColor consoleColor, string value, params object[] arg);
+    }
+}
---
+++ Source/Comsec.Sugar/Command/ParameterParser.cs
@@ -0,0 +1,50 @@
+﻿namespace Comsec.Lib.Console
+{
+    /// <summary>
+    /// Parses a string array to return a argument list.
+    /// </summary>
+    public static class ParameterParser
+    {
+        /// <summary>
+        /// Parses the specified args.
+        /// </summary>
+        /// <param name="args">The args.</param>
+        /// <returns></returns>
+        public static Parameters Parse(string[] args)
+        {
+            var parameters = new Parameters();
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                var argument = new Parameter();
+
+                var arg = args[i];
+
+                if (arg.StartsWith("-"))
+                {
+                    argument.Name = arg;
+
+                    if (i + 1 < args.Length)
+                    {
+                        var nextArg = args[i + 1];
+
+                        if (!nextArg.StartsWith("-"))
+                        {
+                            argument.Value = nextArg;
+
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    argument.Value = args[i];
+                }
+
+                parameters.Add(argument);
+            }
+
+            return parameters;
+        }
+    }
+}
