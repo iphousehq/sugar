2606dda1a6951d1941af3d99ca0501549eaa7922
Chris Wood <chris@flipbit.co.uk>
2011-07-14 14:24:19 +0100

    Changed XML processing to user XPathDocument

---
+++ Source/Comsec.Sugar.Test/Xml/XPathDocumentExtensionsTest.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+
+namespace Comsec.Sugar.Xml
+{
+    [TestFixture]
+    public class XPathDocumentExtensionsTest
+    {            
+        [Test]
+        public void TestToXPath()
+        {
+            var xml = @"<node><value attribute='test'>Hello World</value></node>".ToXPath();
+
+            Assert.IsNotNull(xml);
+        }
+
+        [Test]
+        public void TestGetNamespace()
+        {
+            var xml = @"<node xmlns=""http://localhost/test""><value attribute='test'>Hello World</value></node>".ToXPath();
+
+            var value = xml.GetNamespace();
+
+            Assert.AreEqual("http://localhost/test", value);
+        }
+
+
+        [Test]
+        public void TestGetNamespaces()
+        {
+            var xml = @"<node xmlns:foo=""http://localhost/test1"" xmlns:bar=""http://localhost/test2""><value attribute='test'>Hello World</value></node>".ToXPath();
+
+            var value = xml.GetNamespaces();
+
+            Assert.AreEqual(2, value.Count);
+            Assert.AreEqual("http://localhost/test2", value[0]);
+            Assert.AreEqual("http://localhost/test1", value[1]);
+        }
+
+        [Test]
+        public void TestGetInnerXml()
+        {
+            var xml = @"<node><value attribute='test'>Hello World</value></node>".ToXPath();
+
+            var value = xml.GetInnerXml("//value");
+
+            Assert.AreEqual("Hello World", value);
+
+        }
+
+        [Test]
+        public void TestGetInnerXmlWithNamespace()
+        {
+            var xml = @"<node xmlns=""http://localhost/test""><value attribute='test'>Hello World</value></node>".ToXPath();
+
+            var value = xml.GetInnerXml("//value");
+
+            Assert.AreEqual("Hello World", value);
+        }
+
+
+        [Test]
+        public void TestGetMatches()
+        {
+            var xml = @"<node><value>Hello World 1</value><value>Hello World 2</value></node>".ToXPath();
+
+            var results = xml.GetMatches("//value");
+
+            Assert.AreEqual(2, results.Count);
+            Assert.IsTrue(results[0].CreateNavigator().InnerXml.Contains("Hello World 1"));
+            Assert.IsTrue(results[1].CreateNavigator().InnerXml.Contains("Hello World 2"));
+        }
+
+        [Test]
+        public void TestGetMatchesWithNamespace()
+        {
+            var xml = @"<node xmlns=""http://localhost/test""><value>Hello World 1</value><value>Hello World 2</value></node>".ToXPath();
+
+            var results = xml.GetMatches("//value");
+
+            Assert.AreEqual(2, results.Count);
+            Assert.IsTrue(results[0].CreateNavigator().InnerXml.Contains("Hello World 1"));
+            Assert.IsTrue(results[1].CreateNavigator().InnerXml.Contains("Hello World 2"));
+        }
+    }
+}
---
+++ Source/Comsec.Sugar/Xml/XPathDocumentExtensions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace Comsec.Sugar.Xml
+{
+    /// <summary>
+    /// Extension methods for the <see cref="XPathDocument"/> class.
+    /// </summary>
+    public static class XPathDocumentExtensions
+    {
+        /// <summary>
+        /// Returns an <see cref="XPathDocument"/> representation of this XML string.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static XPathDocument ToXPath(this string value)
+        {
+            return new XPathDocument(new StringReader(value));
+        }
+
+        /// <summary>
+        /// Gets the first namespace in the XML document.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <returns></returns>
+        public static string GetNamespace(this XPathDocument document)
+        {
+            var navigator = document.CreateNavigator();
+
+            navigator.MoveToFollowing(XPathNodeType.Element);
+
+            var namespaces = navigator.GetNamespacesInScope(XmlNamespaceScope.ExcludeXml);
+
+            if (namespaces != null)
+            {
+                foreach (var key in namespaces.Keys)
+                {
+                    return namespaces[key];
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets the namespaces in the XML document.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <returns></returns>
+        public static IList<string> GetNamespaces(this XPathDocument document)
+        {
+            var results = new List<string>();
+
+            var navigator = document.CreateNavigator();
+
+            navigator.MoveToFollowing(XPathNodeType.Element);
+
+            var namespaces = navigator.GetNamespacesInScope(XmlNamespaceScope.ExcludeXml);
+
+            if (namespaces != null)
+            {
+                results.AddRange(namespaces.Keys.Select(key => namespaces[key]));
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Gets the inner XML.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <param name="xpath">The xpath.</param>
+        /// <returns></returns>
+        public static string GetInnerXml(this XPathDocument document, string xpath)
+        {
+            var result = string.Empty;
+
+            var iterator = GetIterator(document, xpath);
+
+            if (iterator.Count > 0 && iterator.MoveNext())
+            {
+                if (iterator.Current != null)
+                {
+                    result = iterator.Current.InnerXml;
+                }
+            }
+
+            return result;
+        }
+
+        private static string AddPrefixToXPath(string xpath, string prefix)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(xpath, @"(^(?![A-Za-z0-9\-\.]+::)|[A-Za-z0-9\-\.]+::|[@|/])(?'Expression'[A-Za-z0-9\-\.]+)", x =>
+            {
+                var expressionIndex = x.Groups["Expression"].Index - x.Index;
+                var before = x.Value.Substring(0, expressionIndex);
+                var after = x.Value.Substring(expressionIndex, x.Value.Length - expressionIndex);
+
+                return String.Format("{0}{1}:{2}", before, prefix, after);
+            });
+        }
+
+        /// <summary>
+        /// Gets a list of navigators from the given XPath.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <param name="xpath">The xpath.</param>
+        /// <returns></returns>
+        public static IList<XPathDocument> GetMatches(this XPathDocument document, string xpath)
+        {
+            var results = new List<XPathDocument>();
+
+            var iterator = GetIterator(document, xpath);
+
+            if (iterator.Count > 0)
+            {
+                while (iterator.MoveNext())
+                {
+                    var result = new XPathDocument(new StringReader(iterator.Current.OuterXml));
+
+                    results.Add(result);
+                }
+            }
+
+            return results;
+        }
+
+        private static XPathNodeIterator GetIterator(XPathDocument document, string xpath)
+        {
+            var navigator = document.CreateNavigator();
+
+            var @namespace = document.GetNamespace();
+
+            var manager = new XmlNamespaceManager(navigator.NameTable);
+
+            if (!string.IsNullOrEmpty(@namespace))
+            {
+                xpath = AddPrefixToXPath(xpath, "pfx");
+
+                manager.AddNamespace("pfx", @namespace);
+            }
+
+            var iterator = navigator.Select(xpath, manager);
+            return iterator;
+        }
+    }
+}
