90c6e6492894226c908de7b23c76690e7b26c738
bounav <benjamin.fourmond@comsechq.com>
2011-11-09 17:35:18 +0000

    Added default parameters to HttpQuery.Get() method.

--- Source/Sugar/Net/HttpQuery.cs
+++ Source/Sugar/Net/HttpQuery.cs
@@ -46,14 +46,15 @@ namespace Sugar.Net
         /// <param name="url">The URL.</param>
         /// <param name="agent">The agent.</param>
         /// <param name="cookies">The cookies.</param>
-        /// <param name="persistState">
-        /// if set to <c>true</c> persist the state (cookes, UserAgent and referring URL) from any previous
-        /// requests.
-        /// </param>
+        /// <param name="persistState">if set to <c>true</c> persist the state (cookes, UserAgent and referring URL) from any previous
+        /// requests.</param>
+        /// <param name="referer">The referer URL.</param>
+        /// <param name="retries">The number of retry attemps.</param>
+        /// <param name="timeout">The timeout in milliseconds.</param>
         /// <returns></returns>
-        public virtual HttpQuery Get(string url, UserAgent agent = null, CookieContainer cookies = null, bool persistState = true)
+        public virtual HttpQuery Get(string url, UserAgent agent = null, CookieContainer cookies = null, bool persistState = true, string referer = null, int retries = 0, int timeout = 2500)
         {
-            Request = HttpService.Build(url, HttpVerb.Get, agent, cookies);
+            Request = HttpService.Build(url, HttpVerb.Get, agent, cookies, referer, retries, timeout);
 
             // Persist cookies across requests
             if (Response != null && persistState)
