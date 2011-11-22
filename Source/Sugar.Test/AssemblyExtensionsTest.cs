b8984294558cbfb1487272d2640c8c44eba7ad09
Chris Wood <chris@flipbit.co.uk>
2011-11-22 13:55:22 +0000

    Adding assembly helper methods

---
+++ Source/Sugar.Test/AssemblyExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Sugar
+{
+    [TestFixture]
+    public class AssemblyExtensionsTest
+    {
+        [Test]
+        public void TestGetTypes()
+        {
+            var types = GetType().Assembly.GetTypes("Sugar.Xml");
+
+            Assert.AreEqual(1, types.Count());
+        }
+    }
+}
---
+++ Source/Sugar/AssemblyExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Sugar
+{
+    /// <summary>
+    /// Assembly extentsion methods
+    /// </summary>
+    public static class AssemblyExtensions
+    {
+        /// <summary>
+        /// Gets the types from this assembly, optionally limiting those returned to Types
+        /// within the specified namespace.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <param name="namespace">The @namespace.</param>
+        /// <returns></returns>
+        public static IEnumerable<Type> GetTypes(this Assembly assembly, string @namespace = "")
+        {
+            IEnumerable<Type> types;
+
+            if (string.IsNullOrEmpty(@namespace))
+            {
+                types = assembly.GetTypes();
+            }
+            else
+            {
+                types = assembly
+                    .GetTypes()
+                    .Where(t => !string.IsNullOrEmpty(t.Namespace) &&
+                                t.Namespace.StartsWith(@namespace));
+            }
+
+            return types;
+        }
+    }
+}
