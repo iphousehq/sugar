dd515b01c660e67936f8ebc915842c21322b06f9
bounav <benjamin.fourmond@comsechq.com>
2012-02-03 11:18:13 +0000

    Added an iphone user agent.

--- Source/Sugar.Test/Net/UserAgentTest.cs
+++ Source/Sugar.Test/Net/UserAgentTest.cs
@@ -11,6 +11,7 @@ namespace Sugar.Net
             Assert.AreEqual("Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)", UserAgent.InternetExplorer().ToString());
             Assert.AreEqual("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0.1) Gecko/20100101 Firefox/8.0.1", UserAgent.Firefox().ToString());
             Assert.AreEqual("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2", UserAgent.Chrome().ToString());
+            Assert.AreEqual("Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5", UserAgent.Iphone().ToString());
             Assert.AreEqual("test", UserAgent.Custom("test").ToString());
         }
     }
--- Source/Sugar/Net/UserAgent.cs
+++ Source/Sugar/Net/UserAgent.cs
@@ -62,6 +62,15 @@ namespace Sugar.Net
         }
 
         /// <summary>
+        /// Return an iOS 4.3.3 Safari user agent.
+        /// </summary>
+        /// <returns></returns>
+        public static UserAgent Iphone()
+        {
+            return new UserAgent("Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5");
+        }
+
+        /// <summary>
         /// Returns a custom user agent
         /// </summary>
         /// <returns></returns>
