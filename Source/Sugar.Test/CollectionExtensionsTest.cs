d54316509c0f591326eab36490877e0d7e6d6b75
Phil <phil.higgins@comsechq.com>
2012-03-13 09:59:51 +0000

    Added new extensions for collections.

--- Source/Sugar.Test/CollectionExtensionsTest.cs
+++ Source/Sugar.Test/CollectionExtensionsTest.cs
@@ -28,5 +28,139 @@ namespace Sugar
 
             Assert.AreEqual(1, list.Count);
         }
+
+        [Test]
+        public void TestGetMaximumIndexUsingFilterNoneMatch()
+        {
+            var collection = new List<String>
+            {
+                "test",
+                "something"
+            };
+
+            var result = collection.GetMaximumIndexUsingFilter(x => x.Length > 10);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void TestGetMaximumIndexUsingFilterOneMatches()
+        {
+            var collection = new List<String>
+            {
+                "test",
+                "something"
+            };
+
+            var result = collection.GetMaximumIndexUsingFilter(x => x.Length > 4);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void TestGetMaximumIndexUsingFilterMultipleMatch()
+        {
+            var collection = new List<String>
+            {
+                "test",
+                "something"
+            };
+
+            var result = collection.GetMaximumIndexUsingFilter(x => x.Length > 2);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void TestGetIndexUsingFilterNotInList()
+        {
+            var collection = new List<String>
+            {
+                "test",
+                "something"
+            };
+
+            var result = collection.GetIndexUsingFilter(x => x == "no");
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void TestGetIndexUsingFilterInList()
+        {
+            var collection = new List<String>
+            {
+                "test",
+                "something"
+            };
+
+            var result = collection.GetIndexUsingFilter(x => x == "test");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void TestRemoveAndInsertOneItemDoesNotExist()
+        {
+            var collection = new List<String>
+            {
+                "test1",
+                "test2",
+                "test3"
+            };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => collection.RemoveAndInsertAt(0, 3));
+        }
+
+        [Test]
+        public void TestRemoveAndInsertItems()
+        {
+            var collection = new List<String>
+            {
+                "test1",
+                "test2",
+                "test3"
+            };
+
+            var results = collection.RemoveAndInsertAt(1, 0);
+
+            Assert.AreEqual("test2", results[0]);
+            Assert.AreEqual("test1", results[1]);
+            Assert.AreEqual("test3", results[2]);
+        }
+
+        [Test]
+        public void TestRemoveAndInsertItemsSameIndex()
+        {
+            var collection = new List<String>
+            {
+                "test1",
+                "test2",
+                "test3"
+            };
+
+            var results = collection.RemoveAndInsertAt(0, 0);
+
+            Assert.AreEqual("test1", results[0]);
+            Assert.AreEqual("test2", results[1]);
+            Assert.AreEqual("test3", results[2]);
+        }
+
+        [Test]
+        public void TestRemoveAndInsertItemsRemoveIndexLowerThanInsert()
+        {
+            var collection = new List<String>
+            {
+                "test1",
+                "test2",
+                "test3"
+            };
+
+            var results = collection.RemoveAndInsertAt(0, 1);
+
+            Assert.AreEqual("test2", results[0]);
+            Assert.AreEqual("test1", results[1]);
+            Assert.AreEqual("test3", results[2]);
+        }
     }
 }
--- Source/Sugar/CollectionExtensions.cs
+++ Source/Sugar/CollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Sugar
 {
@@ -8,14 +9,74 @@ namespace Sugar
         /// Adds an item to the specified list if it is not already contained in the list.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="list">The list.</param>
+        /// <param name="collection">The collection.</param>
         /// <param name="item">The item.</param>
-        public static void AddIfNotExists<T>(this ICollection<T> list, T item)
+        public static void AddIfNotExists<T>(this ICollection<T> collection, T item)
         {
-            if (!list.Contains(item))
+            if (!collection.Contains(item))
             {
-                list.Add(item);
+                collection.Add(item);
             }
+        }
+
+        /// <summary>
+        /// Gets the maximum index of the item in a list that matches the given filter.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The collection.</param>
+        /// <param name="filter">The filter.</param>
+        /// <returns></returns>
+        public static int GetMaximumIndexUsingFilter<T>(this IList<T> collection, Func<T, bool> filter)
+        {
+            var index = -1;
+
+            for (var i = 0; i < collection.Count; i++)
+            {
+                if (filter.Invoke(collection[i]))
+                {
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+
+        /// <summary>
+        /// Gets the index of the first item in the list that matches the given filter.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The collection.</param>
+        /// <param name="filter">The filter.</param>
+        /// <returns></returns>
+        public static int GetIndexUsingFilter<T>(this IList<T> collection, Func<T, bool> filter)
+        {
+            for (var i = 0; i < collection.Count; i++)
+            {
+                if (filter.Invoke(collection[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Swaps the items at the specified indices.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">The columns.</param>
+        /// <param name="removeIndex">Index of the remove.</param>
+        /// <param name="insertIndex">Index of the insert.</param>
+        /// <returns></returns>
+        public static IList<T> RemoveAndInsertAt<T>(this IList<T> collection, int removeIndex, int insertIndex)
+        {
+            var temp = collection[removeIndex];
+
+            collection.RemoveAt(removeIndex);
+            collection.Insert(insertIndex, temp);
+
+            return collection;
         }
     }
 }
