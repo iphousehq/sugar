4be488ec6ee6f43aa4c0e2b7fdae8dcda912ac9f
Phil <phil.higgins@comsechq.com>
2012-08-17 10:39:49 +0100

    Extension method to convert int to an ordinal (1st, 2nd, 3rd,etc).

--- Source/Sugar.Test/Int32ExtensionsTest.cs
+++ Source/Sugar.Test/Int32ExtensionsTest.cs
@@ -10,5 +10,117 @@ namespace Sugar
         {
             Assert.AreEqual(100, "100".AsInt32());
         }
+
+        [Test]
+        public void TestOrdinalMinusOne()
+        {
+            var result = (-1).AddOrdinal();
+
+            Assert.AreEqual("-1", result);
+        }
+
+        [Test]
+        public void TestOrdinalZero()
+        {
+            var result = 0.AddOrdinal();
+
+            Assert.AreEqual("0", result);
+        }
+
+        [Test]
+        public void TestOrdinalOne()
+        {
+            var result = 1.AddOrdinal();
+
+            Assert.AreEqual("1st", result);
+        }
+
+        [Test]
+        public void TestOrdinalTwo()
+        {
+            var result = 2.AddOrdinal();
+
+            Assert.AreEqual("2nd", result);
+        }
+
+        [Test]
+        public void TestOrdinalThree()
+        {
+            var result = 3.AddOrdinal();
+
+            Assert.AreEqual("3rd", result);
+        }
+
+        [Test]
+        public void TestOrdinalFour()
+        {
+            var result = 4.AddOrdinal();
+
+            Assert.AreEqual("4th", result);
+        }
+
+        [Test]
+        public void TestOrdinalEleven()
+        {
+            var result = 11.AddOrdinal();
+
+            Assert.AreEqual("11th", result);
+        }
+
+        [Test]
+        public void TestOrdinalTwelve()
+        {
+            var result = 12.AddOrdinal();
+
+            Assert.AreEqual("12th", result);
+        }
+
+        [Test]
+        public void TestOrdinalThirteen()
+        {
+            var result = 13.AddOrdinal();
+
+            Assert.AreEqual("13th", result);
+        }
+
+        [Test]
+        public void TestOrdinalFourteen()
+        {
+            var result = 14.AddOrdinal();
+
+            Assert.AreEqual("14th", result);
+        }
+
+        [Test]
+        public void TestOrdinalTwentyOne()
+        {
+            var result = 21.AddOrdinal();
+
+            Assert.AreEqual("21st", result);
+        }
+
+        [Test]
+        public void TestOrdinalTwentyTwo()
+        {
+            var result = 22.AddOrdinal();
+
+            Assert.AreEqual("22nd", result);
+        }
+
+        [Test]
+        public void TestOrdinalTwentyThree()
+        {
+            var result = 23.AddOrdinal();
+
+            Assert.AreEqual("23rd", result);
+        }
+
+        [Test]
+        public void TestOrdinalTwentyFour()
+        {
+            var result = 24.AddOrdinal();
+
+            Assert.AreEqual("24th", result);
+        }
     }
 }
--- Source/Sugar/Int32Extensions.cs
+++ Source/Sugar/Int32Extensions.cs
@@ -7,11 +7,22 @@ namespace Sugar
     /// </summary>
     public static class Int32Extensions
     {
+        /// <summary>
+        /// Convert string to integer
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
         public static int AsInt32(this string value)
         {
             return value.AsInt32(0);
         }
 
+        /// <summary>
+        /// Convert string to integer with a default if the string is empty.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="default">The @default.</param>
+        /// <returns></returns>
         public static int AsInt32(this string value, int @default)
         {
             var result = @default;
@@ -23,6 +34,36 @@ namespace Sugar
                 if (int.TryParse(value, out temp))
                 {
                     result = Convert.ToInt32(temp);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adds an ordinal.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string AddOrdinal(this int value)
+        {
+            var result = value.ToString();
+
+            if(value > 0)
+            {
+                switch(value % 100)
+                {
+                    case 11: case 12: case 13: result += "th"; break;
+
+                    default:
+                        switch(value % 10)
+                        {
+                            case 1: result += "st"; break;
+                            case 2: result += "nd"; break;
+                            case 3: result += "rd"; break;
+                            default: result += "th"; break;
+                        }
+                    break;
                 }
             }
 
