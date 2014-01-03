61e16db4111ac73e4a857a2609299a94951927cf
Benjamin Fourmond <benjamin.fourmond@comsechq.com>
2014-01-03 11:10:23 +0000

    Added CountryCode and TypeaheadTokenAttribute from core.

---
+++ Source/Sugar.Test/Attributes/TypeaheadTokenAttributeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sugar.Attributes
+{
+    [TestFixture]
+    public class TypeaheadTokenAttributeTest
+    {
+        [Test]
+        public void TestSingleWord()
+        {
+            var result = new TypeaheadTokenAttribute("gb");
+
+            Assert.AreEqual("gb", result.Token);
+        }
+
+        [Test]
+        public void TestMultipleWordsNotNullOrEmptyValues()
+        {
+            Assert.Throws<ArgumentException>(() => new TypeaheadTokenAttribute(null));
+            Assert.Throws<ArgumentException>(() => new TypeaheadTokenAttribute(""));
+            Assert.Throws<ArgumentException>(() => new TypeaheadTokenAttribute("  "));
+        }
+
+        public void TestMultipleWordsNotAllowMultipleWords()
+        {
+            Assert.Throws<ArgumentException>(() => new TypeaheadTokenAttribute("great britain"));
+        }
+    }
+}
