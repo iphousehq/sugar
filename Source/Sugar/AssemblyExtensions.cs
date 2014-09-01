191822cc4aa8fb5bdad43b3fbce886d121b22b43
Benjamin Fourmond <benjamin.fourmond@comsechq.com>
2014-09-01 13:19:11 +0000

    Simplified GetTypes extension method parameters with the special params keyword.

--- Source/Sugar/AssemblyExtensions.cs
+++ Source/Sugar/AssemblyExtensions.cs
@@ -10,24 +10,12 @@ namespace Sugar
     public static class AssemblyExtensions
     {
         /// <summary>
-        /// Gets the types from this assembly, optionally limiting those returned to Types
-        /// within the specified namespace.
-        /// </summary>
-        /// <param name="assembly">The assembly.</param>
-        /// <param name="namespace">The @namespace.</param>
-        /// <returns></returns>
-        public static IEnumerable<Type> GetTypes(this Assembly assembly, string @namespace)
-        {
-            return assembly.GetTypes(new[] {@namespace});
-        }
-
-        /// <summary>
         /// Gets the types in the <see cref="assembly"/> that have a namespace starting with witht the given <see cref="namespaces"/>.
         /// </summary>
         /// <param name="assembly">The assembly.</param>
         /// <param name="namespaces">The namespaces.</param>
         /// <returns></returns>
-        public static IEnumerable<Type> GetTypes(this Assembly assembly, string[] @namespaces = null)
+        public static IEnumerable<Type> GetTypes(this Assembly assembly, params string[] @namespaces)
         {
             IEnumerable<Type> types;
 
