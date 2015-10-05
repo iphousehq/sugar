b3a279e1ee6429a9595ef5ab43483f3f154d301f
Phil Higgins <phil.higgins@comsechq.com>
2015-10-05 16:18:21 +0100

    [#104698042] Added extension methods to HTTP request message
    - Extension methods to set ACCEPT headers

---
+++ Source/Sugar.Http/HttpRequestMesssageExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http;
+
+namespace Sugar.Http
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpRequestMessage"/>
+    /// </summary>
+    public static class HttpRequestMesssageExtensions
+    {
+        /// <summary>
+        /// Sets the HTTP request accept header to HTML.
+        /// </summary>
+        /// <param name="req">The req.</param>
+        /// <returns>HttpRequestMessage.</returns>
+        public static HttpRequestMessage SetAcceptHeaderToHtml(this HttpRequestMessage req)
+        {
+            req.Headers.Accept.TryParseAdd("text/html");
+
+            return req;
+        }
+
+        /// <summary>
+        /// Sets the HTTP request accept header to JSON.
+        /// </summary>
+        /// <param name="req">The req.</param>
+        /// <returns>HttpRequestMessage.</returns>
+        public static HttpRequestMessage SetAcceptHeaderToJson(this HttpRequestMessage req)
+        {
+            req.Headers.Accept.TryParseAdd("application/json");
+
+            return req;
+        }
+
+        /// <summary>
+        /// Sets the accept encoding HTTP header to accept compressed responses e.g. gzip, deflate.
+        /// </summary>
+        /// <param name="req">The req.</param>
+        /// <returns>HttpRequestMessage.</returns>
+        public static HttpRequestMessage SetAcceptEncodingToCompressed(this HttpRequestMessage req)
+        {
+            req.Headers.AcceptEncoding.TryParseAdd("gzip");
+            req.Headers.AcceptEncoding.TryParseAdd("deflate");
+
+            return req;
+        }
+
+        /// <summary>
+        /// Sets the accept language HTTP header to US English.
+        /// </summary>
+        /// <param name="req">The req.</param>
+        /// <returns>HttpRequestMessage.</returns>
+        public static HttpRequestMessage SetAcceptLanguageToUsEnglish(this HttpRequestMessage req)
+        {
+            req.Headers.AcceptLanguage.TryParseAdd("en-US");
+            req.Headers.AcceptLanguage.TryParseAdd("en");
+
+            return req;
+        }
+    }
+}
---
+++ Source/Sugar.Test/Http/HttpRequestMessageExtensionsTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http;
+using NUnit.Framework;
+
+namespace Sugar.Http
+{
+    [TestFixture]
+    public class HttpRequestMessageExtensionsTest
+    {
+        [Test]
+        public void TestSetAcceptHeaderToHtml()
+        {
+            var req = new HttpRequestMessage();
+
+            req.SetAcceptHeaderToHtml();
+
+            Assert.AreEqual("text/html", req.Headers.Accept.ToString());
+        }
+
+        [Test]
+        public void TestSetAcceptHeaderToJson()
+        {
+            var req = new HttpRequestMessage();
+
+            req.SetAcceptHeaderToJson();
+
+            Assert.AreEqual("application/json", req.Headers.Accept.ToString());
+        }
+
+        [Test]
+        public void TestSetAcceptEncodingToCompressed()
+        {
+            var req = new HttpRequestMessage();
+
+            req.SetAcceptEncodingToCompressed();
+
+            Assert.AreEqual("gzip, deflate", req.Headers.AcceptEncoding.ToString());
+        }
+
+        [Test]
+        public void TestSetAcceptLanguageToUsEnglish()
+        {
+            var req = new HttpRequestMessage();
+
+            req.SetAcceptLanguageToUsEnglish();
+
+            Assert.AreEqual("en-US, en", req.Headers.AcceptLanguage.ToString());
+        }
+    }
+}
