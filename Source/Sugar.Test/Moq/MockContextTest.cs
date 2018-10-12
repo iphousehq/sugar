ba0abe38eb63e43691e12ae3125f91160df582d8
bounav <b.fourmond@gmail.com>
2018-10-12 17:02:10 +0100

    Added MockContextTest

---
+++ Source/Sugar.Test/Moq/MockContextTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace Sugar.Moq
+{
+    [TestFixture]
+    public class MockContextTest
+    {
+        [Test]
+        public void TestMockContext()
+        {
+            var context = new MockContext().AddMock<ICloneable>();
+
+            var mockedObject = context.Of<ICloneable>();
+
+            var clone = new object();
+
+            context.Get<ICloneable>()
+                   .Setup(call => call.Clone())
+                   .Returns(clone);
+
+            var result = mockedObject.Clone();
+
+            Assert.AreSame(clone, result);
+
+            context.Get<ICloneable>()
+                   .Verify(call => call.Clone(), Times.Once());
+        }
+    }
+}
