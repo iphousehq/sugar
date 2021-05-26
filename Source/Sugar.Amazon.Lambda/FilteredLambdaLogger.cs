fb9c9b1c488cfb773f36f7c2eaf3819fb675e702
bounav <benjamin.fourmond@comsechq.com>
2021-05-26 14:54:10 +0100

    New Sugar.Amazon.Lambda project
    - This nuget package can be used to toggle logging on/off in a lambda with an environment variable

---
+++ Source/Sugar.Amazon.Lambda/FilteredLambdaLogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+namespace Sugar.Amazon.Lambda
+{
+    /// <summary>
+    /// Implementation of <see cref="ILambdaLogger"/> that only logs when an environment variable is set to true.
+    /// </summary>
+    public class FilteredLambdaLogger : ILambdaLogger
+    {
+        private readonly bool isLoggingEnabled;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="environmentVariableName">Defaults to "logToCloudwatch" if missing or cannot be parsed to a bool, logging will be enabled.</param>
+        public FilteredLambdaLogger(string environmentVariableName = "logToCloudwatch")
+        {
+            // Defaults to logging if the environment variable is missing
+            var value = Environment.GetEnvironmentVariable(environmentVariableName) ?? "true";
+
+            isLoggingEnabled = bool.Parse(value);
+        }
+
+        /// <summary>
+        /// Logs a message to AWS CloudWatch Logs.
+        /// 
+        /// Logging will not be done:
+        ///  If the role provided to the function does not have sufficient permissions.
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            if (isLoggingEnabled)
+            {
+                LambdaLogger.Log(message);
+            }
+        }
+
+        /// <summary>
+        /// Logs a message, followed by the current line terminator, to AWS CloudWatch Logs.
+        /// 
+        /// Logging will not be done:
+        ///  If the role provided to the function does not have sufficient permissions.
+        /// </summary>
+        /// <param name="message"></param>
+        public void LogLine(string message)
+        {
+            if (isLoggingEnabled)
+            {
+                LambdaLogger.Log(message + Environment.NewLine);
+            }
+        }
+    }
+}
